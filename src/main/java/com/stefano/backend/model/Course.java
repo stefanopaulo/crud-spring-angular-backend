853473a3894cb96a3377b31e616ee93253655a4e
Stefano Souza <stefano.desouza@vmetrix.com>
2021-12-18 16:36:57 -0300

    H2 Database and Connecting Angular to the Spring API

--- src/main/java/com/stefano/backend/model/Course.java
+++ src/main/java/com/stefano/backend/model/Course.java
@@ -5,7 +5,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import lombok.Data;
 
@@ -15,6 +16,7 @@ public class Course {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private Long id;
     
     @Column(length = 200, nullable = false)
